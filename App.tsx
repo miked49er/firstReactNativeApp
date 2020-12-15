22125a1f1d2fa4c1d159ee0a984e5086239dc991
BuildTools <unconfigured@null.spigotmc.org>
2020-12-15 16:36:01 -0500

    Lesson 2.27

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Button, TextInput, View } from 'react-native';
 
 export default function App() {
+    const [enteredGoal, setEnteredGoal] = useState<string>('');
+    const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+    const goalInputHander = (enteredText: string) => {
+        setEnteredGoal(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        setCourseGoals(courseGoals => [...courseGoals, enteredGoal]);
+    };
+
     return (
-        <View
-            style={styles.screen}
-        >
-            <View
-                style={styles.inputContainer}
-            >
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
                 <TextInput
                     placeholder='Course Goal'
                     style={styles.input}
+                    onChangeText={goalInputHander}
+                    value={enteredGoal}
                 />
-                <Button title='ADD'/>
+                <Button
+                    title='ADD'
+                    onPress={addGoalHandler}
+                />
             </View>
             <View>
+
             </View>
         </View>
     );
