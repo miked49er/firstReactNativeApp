90548f4cdea44c72c590b6e18353971e3580c1ca
BuildTools <unconfigured@null.spigotmc.org>
2020-12-16 15:45:47 -0500

    Revert "Lesson 2.24"
    This reverts commit 2a16611b

--- App.tsx
+++ App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Button, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <View
-            style={{
-                padding: 50
-            }}
-        >
-            <View
-                style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                }}
-            >
-                <TextInput
-                    placeholder='Course Goal' style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
-                />
-                <Button title='ADD'/>
-            </View>
-            <View>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
