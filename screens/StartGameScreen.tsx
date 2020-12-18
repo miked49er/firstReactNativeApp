323f10e3f5d65230d1aa7cb133cdfb5b3163c6be
BuildTools <unconfigured@null.spigotmc.org>
2020-12-18 14:25:37 -0500

    Lesson 4.56

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
 interface IStartGameScreenProps {
 
@@ -9,7 +9,23 @@ interface IStartGameScreenProps {
 const StartGameScreen = (props: IStartGameScreenProps) => {
     return (
         <View style={styles.wrapper}>
-            <Text>The Game Screen</Text>
+            <Text style={styles.title}>Start New Game</Text>
+            <View style={styles.card}>
+                <Text>Select a Number</Text>
+                <TextInput/>
+                <View style={styles.actions}>
+                    <Button
+                        title='Reset'
+                        onPress={() => {
+                        }}
+                    />
+                    <Button
+                        title='Confirm'
+                        onPress={() => {
+                        }}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
@@ -19,11 +35,25 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
-    }
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    card: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+
+    },
+    actions: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
 });
 
-StartGameScreen.propTypes = {
-
-};
+StartGameScreen.propTypes = {};
 
 export default StartGameScreen;
