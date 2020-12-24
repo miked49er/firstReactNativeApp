4d038372b92269ecb7ed0b4f4c33a82bb64948c4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-23 20:11:43 -0500

    Lesson 5.94

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import BodyText from '../components/BodyText';
 import HeaderText from '../components/HeaderText';
 import Theme from '../constants/theme';
@@ -14,23 +14,25 @@ interface IGameOverScreenProps {
 
 const GameOverScreen = ({roundsNum, userNumber, onRestart}: IGameOverScreenProps) => {
     return (
-        <View style={styles.wrapper}>
-            <HeaderText>The Game is Over</HeaderText>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={require('../assets/success.png')}
-                    // source={{uri: 'https://cdn.pixabay.com/photo/2016/11/08/05/20/adventure-1807524_960_720.jpg'}}
-                    style={styles.image}
-                />
+        <ScrollView>
+            <View style={styles.wrapper}>
+                <HeaderText>The Game is Over</HeaderText>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require('../assets/success.png')}
+                        // source={{uri: 'https://cdn.pixabay.com/photo/2016/11/08/05/20/adventure-1807524_960_720.jpg'}}
+                        style={styles.image}
+                    />
+                </View>
+                <View style={styles.resultContainer}>
+                    <BodyText style={styles.resultText}>Your phone needed <Text
+                        style={styles.highlight}
+                    >{roundsNum}</Text> rounds to guess the
+                        number <Text style={styles.highlight}>{userNumber}</Text>.</BodyText>
+                </View>
+                <MainButton onPress={onRestart}>NEW GAME</MainButton>
             </View>
-            <View style={styles.resultContainer}>
-                <BodyText style={styles.resultText}>Your phone needed <Text
-                    style={styles.highlight}
-                >{roundsNum}</Text> rounds to guess the
-                    number <Text style={styles.highlight}>{userNumber}</Text>.</BodyText>
-            </View>
-            <MainButton onPress={onRestart}>NEW GAME</MainButton>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -41,13 +43,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.7,
+        borderRadius: Dimensions.get('window').width * 0.7 / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        margin: 30
+        margin: Dimensions.get('window').height / 30
     },
     image: {
         width: '100%',
@@ -55,7 +57,7 @@ const styles = StyleSheet.create({
     },
     resultContainer: {
         marginHorizontal: 30,
-        marginVertical: 15
+        marginVertical: Dimensions.get('window').height / 60
     },
     highlight: {
         color: Theme.primary,
@@ -64,7 +66,7 @@ const styles = StyleSheet.create({
     },
     resultText: {
         textAlign: 'center',
-        fontSize: 20
+        fontSize: Dimensions.get('window').height < 400 ? 16 : 20
     }
 });
 
