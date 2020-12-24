db103552db80c18c83f1cadbd6dd985e0583a614
BuildTools <unconfigured@null.spigotmc.org>
2020-12-24 16:28:43 -0500

    Lesson 6.114

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { OpenSans_400Regular, OpenSans_700Bold, useFonts } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+    const [fontsLoaded, error] = useFonts({
+        OpenSans_400Regular,
+        OpenSans_700Bold
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading/>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
