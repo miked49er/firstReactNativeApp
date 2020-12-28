6f4a6327e2c6e77e448174d3005f8f6ba90349a7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-28 13:51:46 -0500

    Lesson 6.120

--- screens/CategoryMealsScreen.tsx
+++ screens/CategoryMealsScreen.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { NavigationStackProp } from 'react-navigation-stack';
 
 interface ICategoryMealsScreenProps {
-
+    navigation: NavigationStackProp;
 }
 
 const CategoryMealsScreen = (props: ICategoryMealsScreenProps) => {
     return (
         <View style={styles.wrapper}>
             <Text>The Category Meal Screen</Text>
+            <Button
+                title='Go to Details'
+                onPress={() => {
+                    props.navigation.navigate({routeName: 'MealDetail'});
+                }}
+            />
         </View>
     );
 };
@@ -23,7 +30,7 @@ const styles = StyleSheet.create({
 });
 
 CategoryMealsScreen.propTypes = {
-
+    navigation: PropTypes.object.isRequired
 };
 
 export default CategoryMealsScreen;
