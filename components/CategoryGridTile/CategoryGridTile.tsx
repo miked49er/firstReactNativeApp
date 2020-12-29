7d19aa10f160aade7bbadad0f24005f6c5b9559e
BuildTools <unconfigured@null.spigotmc.org>
2020-12-29 14:29:04 -0500

    Lesson 6.133

--- components/CategoryGridTile/CategoryGridTile.tsx
+++ components/CategoryGridTile/CategoryGridTile.tsx
@@ -34,7 +34,8 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        elevation: 5,
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible'
     },
     container: {
         flex: 1,
@@ -46,7 +47,6 @@ const styles = StyleSheet.create({
             height: 2
         },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
