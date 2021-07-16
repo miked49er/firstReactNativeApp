26128103265b6409d50a981597aabd3a741c1f86
BuildTools <unconfigured@null.spigotmc.org>
2021-07-16 12:47:43 -0400

    Lesson 12.236

--- screens/NewPlaceScreen.tsx
+++ screens/NewPlaceScreen.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, ScrollView, Button } from 'react-native';
+import { Colors } from '../constants/Colors';
 
 interface INewPlaceScreenProps {
 
 }
 
 const NewPlaceScreen = (props: INewPlaceScreenProps) => {
-    return (
-        <View style={styles.wrapper}>
+    const [titleValue, setTitleValue] = useState('');
 
-        </View>
+    const titleChangeHandler = (text: string) => setTitleValue(text);
+
+    const savePlaceHandler = () => {
+    };
+
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={titleValue}
+                />
+                <Button
+                    color={Colors.primary}
+                    title='Save Place'
+                    onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    wrapper: {}
+    form: {margin: 30},
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
 });
 
 export default NewPlaceScreen;
